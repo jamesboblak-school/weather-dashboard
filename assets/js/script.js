adbdba2f5198f323e489d2bf70809e2c8bb537c9
jamesboblak <james@skepticalrecords.com>
2021-07-05 13:14:15 -0700

    input form and local storage working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-var project = started;
+// Store user text input to local storage and print to history-list
+
+function getCity() {
+    var cityName =
+        document.getElementById("city-name").value;
+    document.getElementById(
+        "list").innerHTML = cityName;
+    localStorage.setItem("#city-name", cityName);
+    console.log("cityName: " + localStorage.getItem("#city-name"));
+}
