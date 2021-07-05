5c5f361bf52499b0910a9cf0a25eb4b32b57e885
jamesboblak <james@skepticalrecords.com>
2021-07-05 13:49:47 -0700

    first successful API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,15 @@ function getCity() {
         "list").innerHTML = cityName;
     localStorage.setItem("#city-name", cityName);
     console.log("cityName: " + localStorage.getItem("#city-name"));
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=408547a3d52c68bfd087ff18d95b1ca5', {
+  method: 'GET', //GET is the default.
+  credentials: 'same-origin', // include, *same-origin, omit
+  redirect: 'follow', // manual, *follow, error => follow: Automatically follow redirects. Unless otherwise stated the redirect mode is set to follow
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
 }
